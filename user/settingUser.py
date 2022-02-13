1d62eb60482ba2c5802d0c335b8300f5dca67a33
Wenderson Fazan <wenderson@criarenet.com>
2022-02-13 13:12:19 -0300

    remove privateKey

--- user/settingUser.py
+++ user/settingUser.py
@@ -3,16 +3,7 @@ import starkbank
 
 class SettingUserClass:
     # This is only an example of a private key content. You should use your own key.
-    private_key_content = """
-    -----BEGIN EC PARAMETERS-----
-    BgUrgQQACg==
-    -----END EC PARAMETERS-----
-    -----BEGIN EC PRIVATE KEY-----
-    MHQCAQEEIK45++UeTan44MqXmIR+re67lRmk/vgn7b0ttOPAUQQyoAcGBSuBBAAK
-    oUQDQgAEnLqgdo9teZwe5uZSaFCJceGdt8KbJ6c+36rCK9XdqtvQjrWQCHDPnwJ4
-    upLGDwSdRqVrNCIzjU14VPPDDFLeuw==
-    -----END EC PRIVATE KEY-----
-    """
+    private_key_content = ""
 
     # for project users:
     user = starkbank.Project(
